4fda40e9e9244d20eed027c46071d98c4fdd1ba5
mudaser-sayeedi <mudasersayeed@gmail.com>
2022-09-24 21:20:50 +0430

    some project requirment packages has been added

--- src/App.js
+++ src/App.js
@@ -1,26 +1,28 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { Navigate } from "react-router-dom";
+import Home from "./pages/home/home.page";
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const router = createBrowserRouter([
+    {
+      path: "/mudaser-sayeedi-portfolio",
+      element:<Navigate to="/"/>
+    },
+    {
+      path: "/",
+      element: <Navigate to="/home" />,
+      errorElement: <div>err</div>,
+    },
+    {
+      path: "home",
+      element: <Home />,
+    },
+  ]);
+
+  return <RouterProvider router={router} />;
 }
 
 export default App;
---
+++ src/pages/home/home.page.js
@@ -0,0 +1,8 @@
+import React from "react";
+import "./home.page.scss";
+
+const Home = () => {
+  return <div>Home</div>;
+};
+
+export default Home;
